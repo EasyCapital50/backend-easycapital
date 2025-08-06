dc0fece03ac596bbcb4cd63be44c195063b11523
Ashish S <ashishanandhu78@gmail.com>
2025-08-06 20:35:33 +0530

    port adjust

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI).then(() => console.log('✅ MongoDB Conn
 app.use('/records', recordRoutes);
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Server running on ${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '127.0.0.1', () => {
+  console.log(`🚀 Server running on http://127.0.0.1:${PORT}`);
 });
